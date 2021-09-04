edfc66b8d2658e1f453a6d4f5424002499b6f0b2
gpgaliperez <gp.galiperez@gmail.com>
2021-09-04 13:51:02 -0300

    Levanta -Cambia Spring boot version a 2.3.7.RELEASE -Eureka Client ok

--- src/main/java/com/dan/pgm/ribbonbalancer/RibbonBalancerApplication.java
+++ src/main/java/com/dan/pgm/ribbonbalancer/RibbonBalancerApplication.java
@@ -4,11 +4,14 @@ import com.dan.pgm.ribbonbalancer.config.RibbonConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
-@EnableDiscoveryClient
+
 @SpringBootApplication
-@RibbonClient(configuration = RibbonConfiguration.class)
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RibbonClient(name = "RibbonBalancer" ,configuration = RibbonConfiguration.class)
 public class RibbonBalancerApplication {
 
     public static void main(String[] args) {
